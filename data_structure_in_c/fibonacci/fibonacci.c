0bccebc8cf88d7f65c41d7e0ae635a7dcde57871
Anthony Chang <cdplayer0212@gmail.com>
2020-08-21 09:26:08 +0800

    update fibonacci and add linked list functions
    fibonacci: add iteration method
    queue: add linked list function

--- data_structure_in_c/fibonacci/fibonacci.c
+++ data_structure_in_c/fibonacci/fibonacci.c
@@ -5,14 +5,29 @@
 #include	<limits.h>
 #include	<errno.h>
 
-unsigned long fic(unsigned long num)
+unsigned long fic_recursion(unsigned long num)
 {
 	if (num == 0)
 		return 0;
 	else if (num == 1 || num == 2)
 		return 1;
 
-	return fic(num - 1) + fic(num - 2);
+	return fic_recursion(num - 1) + fic_recursion(num - 2);
+}
+
+unsigned long fic_iteration(unsigned long num)
+{
+	unsigned long i;
+	unsigned long n1 = 0;
+	unsigned long n2 = 1;
+	unsigned long result = num;
+	for (i = 2; i <= num; i++) {
+		result = n1 + n2;
+		n1 = n2;
+		n2 = result;
+	}
+	return result;
+
 }
 
 int main(int argc, char *argv[])
@@ -30,8 +45,12 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
-	if (data > 0)
-		printf("%s %lu, result: %lu\n", argv[0], data, fic(data));
+	if (data > 0) {
+		printf("%s %lu, result: %lu\n", argv[0], data,
+						fic_recursion(data));
+		printf("%s %lu, result: %lu\n", argv[0], data,
+						fic_iteration(data));
+	}
 
 	return 0;
 }
